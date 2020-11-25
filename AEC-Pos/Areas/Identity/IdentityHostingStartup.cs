8f0adafd1f280bde14d9c2325f7d96fd561968ac
M Edoka (21750647) <21750647@dut4life.ac.za>
2020-11-25 23:20:58 +0200

    Add project files.

---
+++ AEC-Pos/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AEC_Pos.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AEC_Pos.Areas.Identity.IdentityHostingStartup))]
+namespace AEC_Pos.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ AEC-Pos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AEC_Pos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
